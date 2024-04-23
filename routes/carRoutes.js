fee5118fffd9798b0650fa7f3c88eff09a3c963d
Dhruv-Varshney55 <dhruvvarshney24112002@gmail.com>
2024-04-23 13:00:39 +0530

    locations update

--- routes/carRoutes.js
+++ routes/carRoutes.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const Car = require('../models/Car')
 const router = express.Router();
+// const locations = require('../data/cities.json');
+const fs= require('fs');
+const path = require('path');
+
+const p = path.join(path.dirname(require.main.filename), "data", "cities.json");
 
 router.get("/", (req, res) => {
     res.render("cars/initial");
@@ -10,7 +15,12 @@ router.get("/home", async (req, res) => {
     const { city } = req.query;
     try {
         let cars = await Car.find({});
-        res.render('cars/index', { cars, city });
+        let a = city;
+        a = a.toLowerCase();
+        fs.readFile(p, (err,data)=>{
+            const cities = JSON.parse(data);
+            res.status(200).render('cars/index', { cars, city , locations : cities[a]});
+        })
     }
     catch (e) {
         res.status(500).render('error', { err: e.message });
@@ -19,15 +29,26 @@ router.get("/home", async (req, res) => {
 
 router.get('/cars/:id' , async(req,res)=>{
     try{
-        
         let {id} = req.params;
         let foundCar = await Car.findById(id).populate('reviews');
-        
         res.render('cars/show' , {foundCar});
-        
     }
     catch(e){
         res.status(500).render('error' , {err:e.message});
+    }
+})
+
+router.get('/particular', async (req,res)=>{
+    try{
+        let {loc} = req.query;
+        loc = loc.toLowerCase();
+        fs.readFile(p, (err,data)=>{
+            const cities = JSON.parse(data);
+            res.status(200).render('car/index',{ locations : cities[loc] });
+        })
+    }
+    catch(err){
+        res.status(400).json({message : err.message});
     }
 })
 
